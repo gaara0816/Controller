16eecade844850d5f08b96844710e148b84606d5
liuxin <liuxin@mumatech.cn>
2020-01-16 15:33:21 +0800

    Revert ".集成极光,在传递信息时进行埋点"
    This reverts commit 88a9291ba3e87de9486b66f707d7b74c7f58b7f4.

--- src/android/PolicyService.java
+++ src/android/PolicyService.java
@@ -1,9 +1,7 @@
 package com.mumatech.controller;
 
 import android.app.Service;
-import android.content.Context;
 import android.content.Intent;
-import android.icu.text.SimpleDateFormat;
 import android.os.IBinder;
 import android.os.Message;
 import android.os.Messenger;
@@ -12,22 +10,13 @@ import android.os.Handler;
 import android.util.Log;
 import android.widget.Toast;
 
-import java.util.Date;
-
-import cn.jiguang.analytics.android.api.CountEvent;
-import cn.jiguang.analytics.android.api.JAnalyticsInterface;
-
 public class PolicyService extends Service {
 
     public static final String ACTION_MAIN = "com.mumatech.controller.ACTION_MAIN";
-    private  Context context;
 
     @Override
     public void onCreate() {
         super.onCreate();
-        context=getApplicationContext();
-        JAnalyticsInterface.setDebugMode(true);
-        JAnalyticsInterface.init(getApplicationContext());
         Log.d(this.getPackageName(), "onCreate: Service服务启动了");
     }
 
@@ -39,23 +28,17 @@ public class PolicyService extends Service {
     private Handler mServerHandler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
-            if (msg != null) {
+            if(msg!=null){
                 Intent main = new Intent(ACTION_MAIN);
                 Bundle data = msg.getData();
-                Date currentTime = new Date();
-                SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-                String dateString = formatter.format(currentTime);
-                CountEvent countEvent = new CountEvent(data.getString(ConfigHelper.COMMAND_TYPE));
-                JAnalyticsInterface.onEvent(context, countEvent);
-                countEvent.addKeyValue("deviceId", AndroidUtil.getDeviceID(context)+dateString);
-
                 main.putExtra(ConfigHelper.COMMAND_TYPE, data.getString(ConfigHelper.COMMAND_TYPE));
                 main.putExtra(ConfigHelper.COMMAND_RES_DATA, data.getString(ConfigHelper.COMMAND_RES_DATA));
                 main.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 try {
                     getApplicationContext().startActivity(main);
-                } catch (Exception e) {
-                    Toast.makeText(getApplicationContext(), "请联系工作人员", Toast.LENGTH_SHORT).show();
+
+                }catch (Exception e){
+                    Toast.makeText( getApplicationContext(),"请联系工作人员",Toast.LENGTH_SHORT).show();
                 }
             }
 
--- src/android/PolicyServiceBroadcastReceiver.java
+++ src/android/PolicyServiceBroadcastReceiver.java
@@ -3,31 +3,16 @@ package com.mumatech.controller;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.icu.text.SimpleDateFormat;
 import android.util.Log;
 import android.widget.Toast;
 
-import java.util.Date;
-
-import cn.jiguang.analytics.android.api.CountEvent;
-import cn.jiguang.analytics.android.api.JAnalyticsInterface;
-
 public class PolicyServiceBroadcastReceiver extends BroadcastReceiver {
-
     public static final String TAG = PolicyServiceBroadcastReceiver.class.getSimpleName();
     public static final String ACTION_MAIN = "com.mumatech.controller.ACTION_MAIN";
     @Override
     public void onReceive(Context context, Intent intent) {
-        JAnalyticsInterface.setDebugMode(true);
-        JAnalyticsInterface.init(context);
         Log.e(TAG, intent.getAction());
         Intent main = new Intent(ACTION_MAIN);
-        Date currentTime = new Date();
-        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        String dateString = formatter.format(currentTime);
-        CountEvent countEvent = new CountEvent(intent.getStringExtra(ConfigHelper.COMMAND_TYPE));
-        JAnalyticsInterface.onEvent(context, countEvent);
-        countEvent.addKeyValue("deviceId", AndroidUtil.getDeviceID(context)+dateString);
         main.putExtra(ConfigHelper.COMMAND_TYPE, intent.getStringExtra(ConfigHelper.COMMAND_TYPE));
         main.putExtra(ConfigHelper.COMMAND_RES_DATA, intent.getStringExtra(ConfigHelper.COMMAND_RES_DATA));
         main.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
